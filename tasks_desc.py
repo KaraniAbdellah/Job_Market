66c8d1de0dde1716e6952dca59b203ff3c227ce1
KaraniAbdellah <abdellahkarani@gmail.com>
2026-04-06 22:26:25 +0100

    feat: start backend

---
+++ tasks_desc.py
@@ -0,0 +1,43 @@
+# Tasks
+'''
+    Build An API
+        --> Build a Function That Accept Column andd Predict Salary
+        --> Make COlumn from Json Object
+        --> fast Apis With EndPoints (Accept: Json Object || Return: Prediction Salary)
+    Build WebSite
+    Power BI DashaBaord
+'''
+
+
+# DataSet Desciption
+'''
+    - country
+    - city
+    - Job & Industry:
+        --> occupation: job_name/role (Software Engineer, HR Analyst, ...)
+        --> field: The broader professional domain for the occupation (Technology, ...)
+
+    - Experience & Salary Variables:
+        --> years_of_experience
+        --> salary
+        --> experience
+        --> job field
+        --> occupation type
+        --> location
+
+    - Employment & Education Variables
+        --> employment_type (full_time, part_time, freelance, ...)
+        --> education_level
+
+    - Demographics & Company Variables
+        --> gender
+        --> company_size
+'''
+
+
+# Ressources
+'''
+    https://www.kaggle.com/datasets/kundanbedmutha/job-market-dataset-global
+    https://www.youtube.com/watch?v=U-JlXWDqvco
+'''
+
